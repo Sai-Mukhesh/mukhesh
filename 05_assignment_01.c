e9e94c59d8423411632375de32569f30c8fdf721
Sai-Mukhesh <153903952+Sai-Mukhesh@users.noreply.github.com>
2024-04-06 17:12:02 +0530

    Update 05_assignment_01.c

--- 05_assignment_01.c
+++ 05_assignment_01.c
@@ -1,13 +1,40 @@
-# include<stdio.h>
-# define SIZE 10
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SIZE 10
+
 int main(void) {
-  int a[SIZE] = {1,2,3,5,6,7,8,9};
-  for (int i=0; i<SIZE; i++) 
-    printf("%d \t",a[i] );
-  a[10] = 25;
-  printf("\nafter inserting...\n");
-  for (int i=0; i<SIZE+1; i++) 
-    printf("%d \t",a[i] );
+    int *a = malloc((SIZE+1) * sizeof(int));
+    if(a == NULL) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    int arr[SIZE] = {1,2,3,5,6,7,8,9};
+    for (int i=0; i<SIZE; i++) {
+        a[i] = arr[i];
+        printf("%d \t",a[i]);
+    }
+
+    a[SIZE] = 25;
+    printf("\nafter inserting...\n");
+    for (int i=0; i<SIZE+1; i++) {
+        printf("%d \t",a[i]);
+    }
+
+    free(a);
+    return 0;
 }
 
-// Static allocation of array will lead to run time errors while inserting new elements.  Alternatively we must use dynamic memory allocation - malloc()
+/*
+
+ observations :
+
+The code dynamically allocates memory for an array a of integers with a size of SIZE + 1.
+It checks if the memory allocation was successful and prints a message if it failed.
+An array arr with SIZE elements is initialized with some values.
+The values from arr are copied to the dynamically allocated array a.
+An additional element is inserted into a at index SIZE.
+The elements of a, including the newly inserted element, are printed before and after the insertion.
+Finally, the dynamically allocated memory is freed to avoid memory leaks.
+*/
